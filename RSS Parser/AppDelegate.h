87e7df26905e9fa96ecf622ae46f86bd898f0299
Roman <a99dneyleta@Romans-MacBook-Air.local>
2017-01-31 18:43:31 +0200

    Initial Commit

---
+++ RSS Parser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RSS Parser
+//
+//  Created by Roman on 1/31/17.
+//  Copyright © 2017 Roman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RSS Parser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RSS Parser
+//
+//  Created by Roman on 1/31/17.
+//  Copyright © 2017 Roman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
