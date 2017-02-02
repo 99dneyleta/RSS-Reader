ed01ce7fdf1f48702484a53e699a83b3e55494ab
Roman <a99dneyleta@Romans-MacBook-Air.local>
2017-02-02 16:36:47 +0200

    RSS Reader

---
+++ RSS Parser/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  RSS Parser
+//
+//  Created by Roman on 2/2/17.
+//  Copyright © 2017 Roman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
